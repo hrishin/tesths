7de939875fbdbbf49b6d39530b55382ef8ccd054
hrishin <hrishishinde.p@gmail.com>
2022-04-03 04:39:30 +0100

    Updates to threejs

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.136.0/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/loaders/GLTFLoader.js';
 
-var textures = ['1.jpg', '2.jpg', '3.jpg','4.jpg', '5.jpg', '6.jpg','7.jpg', '8.jpg', '9.jpg','10.jpg', '11.gif']
+var textures = ['1.jpg', '2.jpg', '3.jpg','4.jpg', '5.jpg', '6.jpg','7.jpg', '8.jpg', '9.jpg','10.jpg', '11.jpg',
+              '12.jpg', '13.jpg','14.jpg', '15.jpg', '16.jpg','17.jpg', '18.jpg', '19.jpg','20.jpg', '21.jpg'];
+// var textures = ['1.jpg', '2.jpg', '3.jpg','4.jpg'];
+var texture;
 var x = -0.140, y=0.610, z=2.16499, aa=0, ss=1.3049, dd=0, toggle=false, t, allTextures, song;
 const SPEED = 0.0015;
 //seconds
-const FIRST_ROTATION =32;
-const NEXT_ROTATIONS =15;
-const rotor = Array.from(Array(5).keys())
+const FIRST_ROTATION = 30;
+const NEXT_ROTATIONS = 19;
+const rotor = Array.from(Array(4).keys());
 var currentRotor = 0
 
 const sizes = {
@@ -16,15 +19,19 @@ const sizes = {
 }
 
 window.addEventListener('load', function() {
-  console.log("document.onload");
-  main();
+  console.log("document onload");
+  $(document).on('click', function () { 
+    $(".info").hide();
+    $("h1.loading").show();
+    main();
+  });
 });
 
 function main() {
   var world;
   const canvas = document.querySelector(".webgl");
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color( 0xffffff );
+  scene.background = new THREE.Color(0xffffff );
 
   const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 1000);
   scene.add(camera);
@@ -45,6 +52,9 @@ function main() {
   const listener = new THREE.AudioListener();
   const sound = new THREE.Audio( listener );
   const audioLoader = new THREE.AudioLoader();
+
+  const video = document.getElementById( 'video' );
+  video.play();
 
   Promise.all([
     loader.loadAsync('me_1.glb'),
@@ -73,8 +83,11 @@ function main() {
     const texLoaders = textures.map(function(url) { return texLoader.load(url)})
     Promise.all(texLoaders)
     .then((results) => {
+      texture = new THREE.MeshLambertMaterial({map: new THREE.VideoTexture( video ), side: THREE.DoubleSide});
       allTextures = results.map(function(t){ return new THREE.MeshStandardMaterial({map: t, side: THREE.DoubleSide})})
       console.log('loaded all textures', allTextures);
+      $(".webgl").show();
+      $("h1.loading").hide();
       startAudio();
       startScene();
       animate();
@@ -113,7 +126,7 @@ function main() {
     const materials = [
       allTextures[3],
       allTextures[1],
-      allTextures[4],
+      texture,
       new THREE.MeshPhongMaterial({color: 'rgb(245,245,245)', side:THREE.DoubleSide}),
       allTextures[2],
       allTextures[0]
@@ -133,7 +146,7 @@ function main() {
     const materials = [
       allTextures[rotor[3]],
       allTextures[rotor[1]],
-      allTextures[rotor[4]],
+      texture,
       new THREE.MeshPhongMaterial({color: 'rgb(245,245,245)', side:THREE.DoubleSide}),
       allTextures[rotor[2]],
       allTextures[rotor[0]]
@@ -174,19 +187,19 @@ function main() {
     const nextTextureIndex = textureIndex + rotor.length;
 
     if (nextTextureIndex > allTextures.length -1) {
-      rotor[currentRotor] = nextTextureIndex % 5;
+      rotor[currentRotor] = nextTextureIndex % rotor.length;
     } else {
       rotor[currentRotor] = nextTextureIndex;
     }
-
-    console.log(rotor);
-    
-    const nextRotor =  currentRotor + 1;
+    console.log(rotor, currentRotor);
+    const nextRotor = currentRotor + 1;
     if (nextRotor > rotor.length-1) {
       currentRotor = 0;
-      return;
-    } 
-    currentRotor = nextRotor;    
+    } else {
+      currentRotor = nextRotor;    
+    }
+
+    
   }
 
   function keyDown(event)
